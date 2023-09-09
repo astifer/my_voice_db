22e5645d17bc46b2fd9da5b3ab07c746c9e888ad
cradmlozzer <artem_pereverzev_04@bk.ru>
2023-09-09 08:50:55 +0300

    back init

---
+++ back/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI, File, UploadFile
+
+app = FastAPI()
+
+
+@app.post('/')
+async def json_file(file: UploadFile):
+    try:
+        contents = file.file.read()
+        with open(file.filename, 'wb') as f:
+            f.write(contents)
+    except Exception:
+        return {"message": "There was an error uploading the file"}
+    finally:
+        file.file.close()
+
+    return {"message": f"Successfully uploaded {file.filename}"}
