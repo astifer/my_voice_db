c40df0eb80cd9991a8e68549c99fc5212bd53f6c
Timur <tz220404@gmail.com>
2023-09-09 12:43:14 +0300

    fixed w2v

--- cluster/w2v.py
+++ cluster/w2v.py
@@ -1,31 +1,42 @@
 from gensim.models import Word2Vec
 import numpy as np
+import json
 
 from create_all_data import all_json_to_df
 
 
-def file_2_vectors(file, question, corpus) -> list:
+def file_2_vectors(file) -> list:
     """
 
-    :params corpus:
-            question:
-            file:
-    :return list:
+    :params file: json-file
+    :return embeddings list:
     """
-    data_all = all_json_to_df(file)
+    answers = []
+    with open(file, encoding='utf_8') as f:
+        q_a = json.loads(f.read())
+        q = q_a["question"]
+        for a in q_a["answers"]:
+            answers.append([a["answer"], q])
+
+    corpus = []
+    for sentence in answers:
+        corpus.append(sentence[0].split())
     model = Word2Vec(corpus, vector_size=100, min_count=1)
 
     vectors = []
-    for sentence in data_all[data_all["question"] == question]["answer"]:
-        if len(sentence) == 0:
+    for sentence in answers:
+        if len(sentence[0]) == 0:
             word_vec = [0] * 100
-        elif sentence not in model.wv.key_to_index.keys():
+        elif sentence[0] not in model.wv.key_to_index.keys():
             w_vs = []
-            for word in sentence.split():
+            for word in sentence[0].split():
                 w_vs.append(model.wv[word])
             word_vec = np.mean(np.array(w_vs), axis=0)
         else:
-            word_vec = model.wv[sentence]
+            word_vec = model.wv[sentence[0]]
         vectors.append(word_vec)
 
     return vectors
+
+#if __name__ == "__main__":
+    # print(file_2_vectors("data/1704.json"))
