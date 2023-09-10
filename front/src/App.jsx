000be7ffc8e694ebf17b3ac75f5807b06d2f50b1
trueprogr <ilovememesdintkillme@gmail.com>
2023-09-10 16:32:31 +0900

    last push

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -7,15 +7,18 @@ const App = () => {
   const [clusteredData, setClusteredData] = useState([]);
 
   useEffect(() => {
-    if (data.length > 0) {
+    if (data.answer && data.text_labels && data.labels) {
       const numClusters = 3;
-      const inputData = Object.keys(data[0].answer).map((key) => [data[0].answer[key], data[0].labels[key]]);
+      const inputData = Object.keys(data.answer).map((key) => [
+        data.answer[key],
+        data.labels[key],
+      ]);
       const kmeans = new KMeans();
       const clusters = kmeans.cluster(inputData, { K: numClusters });
-      const clusteredData = Object.keys(data[0].answer).map((key, index) => ({
-        answer: data[0].answer[key],
-        labels: data[0].labels[key],
-        cluster: clusters[index].centroidIndex,
+      const clusteredData = Object.keys(data.answer).map((key, index) => ({
+        answer: data.answer[key],
+        labels: data.text_labels[key],
+        cluster: data.labels[key],
       }));
       setClusteredData(clusteredData);
     }
@@ -36,9 +39,11 @@ const App = () => {
       });
 
       if (response.ok) {
-        const data = await response.json();
-        if (Array.isArray(data) && data.length > 0 && data[0].answer && data[0].labels) {
-          setData(data);
+        const responseData = await response.json();
+        const { answer, labels, text_labels } = responseData;
+
+        if (answer && labels && text_labels) {
+          setData({ answer, labels, text_labels });
           console.log('File uploaded successfully');
         } else {
           console.error('Invalid data structure');
@@ -51,25 +56,70 @@ const App = () => {
     }
   };
 
+  const groupByCluster = () => {
+    const groupedData = clusteredData.reduce((result, item) => {
+      if (!result[item.cluster]) {
+        result[item.cluster] = [];
+      }
+      result[item.cluster].push(item);
+      return result;
+    }, {});
+    return Object.values(groupedData);
+  };
+
   return (
     <div>
       <input type="file" onChange={handleFileChange} />
       <button onClick={handleUpload}>Upload</button>
 
       {clusteredData.length > 0 && (
-        <div>
-          <h2>Clustering Results</h2>
-          {clusteredData.map((item, index) => (
-            <div key={index}>
-              <p>Answer: {item.answer}</p>
-              <p>Labels: {item.labels}</p>
-              <p>Cluster: {item.cluster}</p>
+        <div style={styles.containingContainer}>
+          <h2>результат кластеризации</h2>
+          {groupByCluster().map((cluster, index) => (
+            <div key={index} style={styles.clusterContainer}>
+              {cluster.map((item, itemIndex) => (
+                <div key={itemIndex} style={styles.itemContainer}>
+                  <p>Ответ: {item.answer}</p>
+                  <p>Кластер: {item.labels}</p>
+                </div>
+              ))}
             </div>
           ))}
         </div>
       )}
     </div>
   );
+};
+
+const styles = {
+
+  containingContainer:{
+  },
+  clusterContainer: {
+    display:'flex',
+    justifyContent:'flex-start',
+    flexDirection:'row',
+    flexWrap:'wrap',
+    marginBottom: '10px',
+    padding: '5px',
+    border: '1px solid #ccc',
+    borderRadius: '5px',
+    backgroundColor:''
+  },
+  clusterHeader: {
+    width:'full',
+    fontSize: '18px',
+    fontWeight: 'bold',
+    marginBottom: '5px',
+  },
+  itemContainer: {
+    marginBottom: '5px',
+    padding: '5px',
+    margin:'5px',
+    border: '1px solid #eee',
+    borderRadius: '3px',
+    backgroundColor: '#f9f9f9',
+  },
 };
 
 export default App;
